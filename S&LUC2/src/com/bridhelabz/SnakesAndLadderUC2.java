d24989ea9475823cfb04c189239d104978d6734d
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-24 17:01:36 +0530

    ADDED

---
+++ S&LUC2/src/com/bridhelabz/SnakesAndLadderUC2.java
@@ -0,0 +1,14 @@
+package com.bridhelabz;
+
+public class SnakesAndLadderUC2 {
+
+	public static void main(String[] args) {
+		var points = 0;
+	int die_number=(int)(Math.floor(Math.random() *10) % 6 )+1;
+	System.out.println(die_number);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
